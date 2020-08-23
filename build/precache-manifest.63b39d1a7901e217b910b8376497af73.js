6210d68e42209ba828e62b76fdabd71f2a06a1a5
sponge2984 <ga576895@gmail.com>
2020-08-23 14:25:38 +0800

    add app

---
+++ build/precache-manifest.63b39d1a7901e217b910b8376497af73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8219e8fa9944677ec7a3f2bb8c421366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6687d5118fe55d96f90",
+    "url": "/static/css/main.4f717267.chunk.css"
+  },
+  {
+    "revision": "7e0c8ea19c40e4daf451",
+    "url": "/static/js/2.a418f3c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a418f3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6687d5118fe55d96f90",
+    "url": "/static/js/main.951e14f0.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
